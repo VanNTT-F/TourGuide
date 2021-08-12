14d5370033359caed86ce43b3e83cf4955fa7546
VanNTT-F <vannttfx07913@funix.edu.vn>
2021-08-12 15:06:00 +0700

    1st amend

--- app/src/main/java/funix/prm/project/DetailList.java
+++ app/src/main/java/funix/prm/project/DetailList.java
@@ -2,11 +2,13 @@ package funix.prm.project;
 
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class DetailList extends MainActivity {
+public class DetailList extends AppCompatActivity {
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
